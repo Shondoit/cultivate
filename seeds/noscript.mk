655b500d3b9d70fb7dfa817d2a9c78013566cbf5
Shondoit Walker <shondoit@gmail.com>
2012-07-13 01:26:48 +0200

    Add NoScript seed.

---
+++ seeds/noscript.mk
@@ -0,0 +1,7 @@
+noscript:=NOSCRIPT
+
+NOSCRIPT_VER:=2.4.7
+NOSCRIPT_PKG:=noscript-$(NOSCRIPT_VER).xpi
+NOSCRIPT_URI:=https://secure.informaction.com/download/releases/$(NOSCRIPT_PKG)
+#NOSCRIPT_URI:=https://addons.mozilla.org/firefox/downloads/latest/722/addon-722-latest.xpi
+NOSCRIPT_HASH:=0f29f46ff1b0db5bd63c604ec6a7f9e1599df847555342ed4f4594f5a093e94c
