d7ee24bbef830890c82776bd4cb477c03a6e0d03
Shondoit Walker <shondoit@gmail.com>
2012-07-13 01:27:45 +0200

    Add TorButton seed.

---
+++ seeds/torbutton.mk
@@ -0,0 +1,8 @@
+torbutton:=TORBUTTON
+
+TORBUTTON_VER:=1.4.6
+TORBUTTON_PKG:=torbutton-$(TORBUTTON_VER).xpi
+TORBUTTON_URI:=https://archive.torproject.org/tor-package-archive/torbutton/$(TORBUTTON_PKG)
+TORBUTTON_HASH:=1d1cc8069bcde695a29504608f3da72f12bc6863270e9316a86adaf5b930cdde
+TORBUTTON_SIG:=https://archive.torproject.org/tor-package-archive/torbutton/$(TORBUTTON_PKG).asc
+TORBUTTON_KEYS:=0xDDC6C0AD
